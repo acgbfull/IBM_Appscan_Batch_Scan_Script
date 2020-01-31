9f8d335dbeb9bf83187409399c027560a331f619
acgbfull <acgb@protonmail.com>
2020-01-31 20:06:34 +0800

    2.0

--- README.md
+++ README.md
@@ -14,7 +14,12 @@
 
 ## Configuration Guide
 配置参数位于ibm_appscan_batch_scan_script.bat的上部，存在注释，请根据实际设置参数值。
-templet.scant（已配置为快速扫描）为AppScan的扫描配置，需要变更请于AppScan的图形化界面进行扫描配置后导出使用。
+
+9.0.3.11_templet.scant（已配置为标准扫描）为AppScan的扫描配置，需要变更请于AppScan的图形化界面进行扫描配置后导出使用。
+
+targets.txt中放入需扫描目标。
+
+默认results文件夹下存放扫描结果。
 
 
 ## Installation Guide
@@ -24,10 +29,11 @@ templet.scant（已配置为快速扫描）为AppScan的扫描配置，需要变
 
 ## Usage
 > 根据实际于ibm_appscan_batch_scan_script.bat中设置好参数的值
-> 
+> 需要扫描的目标放入targets.txt
 > 直接运行ibm_appscan_batch_scan_script.bat
 
 
 ## Change Log
 
 > 2019/02/15  version:1.0.0
+> 2020/01/31  version:2.0.0
--- ibm_appscan_batch_scan_script.bat
+++ ibm_appscan_batch_scan_script.bat
@@ -9,13 +9,14 @@ echo ---------------------------------------------------------------------------
 rem 需要扫描的目标列表路径
 set targets=.\targets.txt
 rem AppScanCMD.exe路径
-set appscancmd=F:\program\appscan_standard_9.0.3.11\AppScanCMD.exe
+set appscancmd="F:\program\appscan_standard_9.0.3.11\AppScanCMD.exe"
 rem 扫描模板路径 
 set templet_path=.\template\9.0.3.11_templet.scant
 rem 扫描结果保存目录
 set save_dir=.\results\
 
 
+mkdir %save_dir% 2>nul
 rem 提醒用户保存好已存在的扫描结果
 for /f  %%j in ('dir /b %save_dir%') do (
 if exist %save_dir%%%j (
@@ -31,7 +32,7 @@ rem 然后新建appscan扫描任务并自动导出pdf报告
 for /f "tokens=1,2,* delims=/" %%k in (%targets%) do (
 set url=%%k//%%l/%%m
 ping -n 6 %%l
-"%appscancmd%" /e /su !url! /st %templet_path% /d %save_dir%%%l.scan /rf %save_dir%%%l安全报告.pdf /rt pdf /v
+%appscancmd% /e /su !url! /st %templet_path% /d %save_dir%%%l.scan /rf %save_dir%%%l安全报告.pdf /rt pdf /v
 echo --------------------------------------------------------------------------------
 )
 del RCL_API_Log_Sample*
